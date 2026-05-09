7fd9a0f2045fa34410994e79f9cfb5b06cab951a
Lenny Schift <lennyschift@gmail.com>
2026-05-09 13:49:40 +0000

    Add Fertigungkarte DocType

---
+++ cnc_hohenstein/cnc_hohenstein/doctype/fertigungkarte/fertigungkarte.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Lenny Schift and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Fertigungkarte", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ cnc_hohenstein/cnc_hohenstein/doctype/fertigungkarte/fertigungkarte.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Lenny Schift and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Fertigungkarte(Document):
+	pass
